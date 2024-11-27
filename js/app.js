e54aa0a402643f0d10cbcb69e542e7d54442a8b5
JosDevelopment <jose.domains.dev@gmail.com>
2024-11-27 11:35:57 -0600

    este si

--- js/app.js
+++ js/app.js
@@ -140,7 +140,7 @@ document.addEventListener('DOMContentLoaded', () => {
     // Handle mouse events
     const handleMouseOver = (event, d) => {
         d3.select(event.target)
-            .attr("fill", "#3498db");
+            .attr("fill", "#006");
     };
 
     const handleMouseOut = (event) => {
